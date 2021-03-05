705c6c5b73b4ffa9f9f3afa497a14d357347df53
Gaetano Antonicchio <71822148+gaetanoantonicchio@users.noreply.github.com>
2021-03-05 22:47:21 +0100

    - tested insertion sort - started writing the notebook insertion_sort_vs_selection_sort_analysis.ipynb

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -5,6 +5,15 @@ def insertion_sort(A):
         while i >= 0 and A[i] > key:
             A[i + 1] = A[i]
             i = i - 1
-        A[i+1] = key
+        A[i + 1] = key
     return A
 
+
+def main():
+    list = [3, 6, 2, 8, 1, 10, 4, 5]
+    print(insertion_sort(list))
+    assert insertion_sort(list) == sorted(list)
+
+
+if __name__ == '__main__':
+    main()
