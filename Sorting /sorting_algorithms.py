7b2fb4eab1e595cbac175ad860fa6bfac6ddc076
Gaetano Antonicchio <71822148+gaetanoantonicchio@users.noreply.github.com>
2021-03-05 23:06:46 +0100

    - selection sort

--- Sorting /sorting_algorithms.py
+++ Sorting /sorting_algorithms.py
@@ -17,3 +17,25 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def selection_sort(A):
+    for j in range(len(A)):
+        min_index = j
+        for i in range(j + 1, len(A)):
+            # to sort in descending order, change > to < in this line
+            # select the minimum element in each loop
+            if A[i] < A[min_index]:
+                min_index = i
+        A[j], A[min_index] = A[min_index], A[j]
+    return A
+
+
+def main():
+    lst = [3, 6, 2, 8, 1, 10, 4, 5]
+    print(selection_sort(lst))
+    # assert selection_sort(lst) == sorted(lst)
+
+
+if __name__ == '__main__':
+    main()
