56d6facd2898a89552935cb95ee95c18cd221f5d
Gaetano Antonicchio <71822148+gaetanoantonicchio@users.noreply.github.com>
2021-03-06 18:29:51 +0100

    fix

--- Sorting /insertion_sort.py
+++ Sorting /insertion_sort.py
@@ -113,25 +113,11 @@ def test_IS(sort_lists, rev_sort_lists, random_lists):
 
 
 '''
-def selection_sort(A):
-    for j in range(len(A)):
-        min_index = j
-        for i in range(j + 1, len(A)):
-            # to sort in descending order, change > to < in this line
-            # select the minimum element in each loop
-            if A[i] < A[min_index]:
-                min_index = i
-        A[j], A[min_index] = A[min_index], A[j]
-    return A
-
-
 def main():
     lst = [3, 6, 2, 8, 1, 10, 4, 5]
     print("unsorted list:", lst)
-    print(f"selection sort:{selection_sort(lst)}")
-    # assert selection_sort(lst) == sorted(lst)
+    assert insertion_sort(lst) == sorted(lst)
     print(f"insertion sort:{insertion_sort(lst)}")
-    # assert selection_sort(lst) == sorted(lst)
 
 
 if __name__ == '__main__':
